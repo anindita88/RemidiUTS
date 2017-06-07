e583ee41d3d1f49cc4b19eedac20b943fc230e21
Anindita <anindita.ekasuci88@gmail.com>
2017-06-07 16:56:44 +0700

    Add navigation button

--- app/src/main/java/xir4n05/remidiuts/telkomschid/remidiuts/MainActivity.java
+++ app/src/main/java/xir4n05/remidiuts/telkomschid/remidiuts/MainActivity.java
@@ -2,12 +2,20 @@ package xir4n05.remidiuts.telkomschid.remidiuts;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSwipe, btnTabs, btnSpinner, btnDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSwipe = (Button) findViewById(R.id.swipe);
+        btnTabs = (Button) findViewById(R.id.tabs);
+        btnSpinner = (Button) findViewById(R.id.spinner);
+        btnDrawer = (Button) findViewById(R.id.drawer);
+
     }
 }
